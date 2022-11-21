f140c99d0ec92ee007800b1a27d2379035d6db56
Joseph Zhang <67695855+zs70108@users.noreply.github.com>
2022-11-21 20:55:32 +0800

    Update piccut.py
    图片分割 修复不能 切 jpg 的bug

--- piccut.py
+++ piccut.py
@@ -1,5 +1,10 @@
 import os
-from PIL import Image
+# python3 -m pip install pillow
+try:
+    from PIL import Image
+except ImportError:
+    os.system("python -m pip install pillow")
+    from PIL import Image
 
 def splitimage(src, rownum, colnum, dstpath):
     img = Image.open(src)
@@ -17,7 +22,7 @@ def splitimage(src, rownum, colnum, dstpath):
         for r in range(rownum):
             for c in range(colnum):
                 box = (c * colwidth, r * rowheight, (c + 1) * colwidth, (r + 1) * rowheight)
-                img.crop(box).save(os.path.join(dstpath, basename + '_' + str(num) + '.' + ext), ext)
+                img.crop(box).save(os.path.join(dstpath, basename + '_' + str(num) + '.' + ext))
                 num = num + 1
     else:
         print('长宽不满足切割要求!')
@@ -25,7 +30,7 @@ def splitimage(src, rownum, colnum, dstpath):
 if __name__ == '__main__':
     src = input('图片文件：')
     if os.path.isfile(src):
-        dstpath = input('图片输出目录')
+        dstpath = input('图片输出目录：')
         if (dstpath == '') or os.path.exists(dstpath):
             row = int(input('行数：'))
             col = int(input('列数：'))
@@ -37,4 +42,3 @@ if __name__ == '__main__':
             print('输出不存在！' % dstpath)
     else:
         print('图片不存在！{}'.format(src))
-
